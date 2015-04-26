453e87de7e6ce8a4eb0422aef9ea99cf9b08899b
sofwarejimenez <sofwarejimenezz@gmail.com>
2015-04-26 11:24:15 +0200

    comentarios

--- js/earth_moon.js
+++ js/earth_moon.js
@@ -2,47 +2,38 @@
 function main() {
 
     function draw() {
-        GL.viewport(0.0, 0.0, CANVAS.width, CANVAS.height);
+        GL.viewport(0.0, 0.0, CANVAS.width, CANVAS.height);//definimos el viewport
         GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT);
-        GL.uniformMatrix4fv(shaders._Pmatrix, false, PROJMATRIX);
-        GL.uniformMatrix4fv(shaders._Vmatrix, false, VIEWMATRIX);
-        GL.uniformMatrix4fv(shaders._Mmatrix, false, MOVEMATRIX);
-        if (cube_texture.webglTexture) {
-
+        GL.uniformMatrix4fv(shaders._Pmatrix, false, PROJMATRIX);//le pasamos al shader matriz de proyeccion
+        GL.uniformMatrix4fv(shaders._Vmatrix, false, VIEWMATRIX);//le pasamos al shader matriz de vista
+        GL.uniformMatrix4fv(shaders._Mmatrix, false, MOVEMATRIX);//le pasamos al shader matriz de modelado
+        if (cube_texture.webglTexture) {//le pasamos la textura
             GL.activeTexture(GL.TEXTURE0);
-
             GL.bindTexture(GL.TEXTURE_2D, cube_texture.webglTexture);
         }
-        GL.bindBuffer(GL.ARRAY_BUFFER, modelo.CUBE_VERTEX);
-        GL.vertexAttribPointer(shaders._position, 3, GL.FLOAT, false, 4 * (3 + 2), 0);
-        GL.vertexAttribPointer(shaders._uv, 2, GL.FLOAT, false, 4 * (3 + 2), 3 * 4);
+        GL.bindBuffer(GL.ARRAY_BUFFER, modelo.CUBE_VERTEX);//tomamos el identificador del buffer de vertices
+        GL.vertexAttribPointer(shaders._position, 3, GL.FLOAT, false, 4 * (3 + 2), 0);//le pasamos al shader las coordenadas de lso vertices
+        GL.vertexAttribPointer(shaders._uv, 2, GL.FLOAT, false, 4 * (3 + 2), 3 * 4);//le pasmaos al shader las coordenadas de textrua
 
-        GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, modelo.CUBE_FACES);
-        GL.drawElements(GL.TRIANGLES, 6 * 2 * 3, GL.UNSIGNED_SHORT, 0);
+        GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, modelo.CUBE_FACES);//tomamos el identificador del buffer de las caras
+        GL.drawElements(GL.TRIANGLES, 6 * 2 * 3, GL.UNSIGNED_SHORT, 0);//dibujamos.
     }
 
 
     //variables necesarias
     var CANVAS, GL;
     CANVAS = LIBS.editCanvas(CANVAS, "your_canvas");
-
+    GL = LIBS.getContext(CANVAS);
     /*========================= MATRIX ========================= */
+    //Definimos matriz de proyeccion, modelo y vista
     var PROJMATRIX = LIBS.get_projection(40, CANVAS.width / CANVAS.height, 1, 100);
     var MOVEMATRIX = LIBS.get_I4();
     var VIEWMATRIX = LIBS.get_I4();
-
-    LIBS.translateZ(VIEWMATRIX, -6);
-
+    LIBS.translateZ(VIEWMATRIX, -6);//traslademos el mundo al (0,0,-6)
     raton.gestionarEventos(CANVAS);
-
-    GL = LIBS.getContext(CANVAS);
-
     shaders.gestionShaders(GL);
-
     modelo.defModelo(GL);
     var cube_texture = texture.get_texture("img/texture.png",GL);
-
-
     /*========================= DRAWING ========================= */
     GL.enable(GL.DEPTH_TEST);
     GL.depthFunc(GL.LEQUAL);
@@ -50,13 +41,13 @@ function main() {
     GL.clearDepth(1.0);
 
     var time_old = 0;
-
     function animate(time) {
         var dt = time - time_old;
-        if (!raton.drag) {
+        if (!raton.drag) {//gestionamos el movimiento del raton
             raton.dX *= raton.AMORTIZATION, raton.dY *= raton.AMORTIZATION;
             raton.THETA += raton.dX, raton.PHI += raton.dY;
         }
+        //modificmoas matriz de modelado
         LIBS.set_I4(MOVEMATRIX);
         LIBS.rotateY(MOVEMATRIX, raton.THETA);
         LIBS.rotateX(MOVEMATRIX, raton.PHI);
@@ -67,6 +58,4 @@ function main() {
         //si queremos que no se anume window.requestAnimationFrame(animate(0));
     }
     animate(0);
-
-
 }
--- js/libs.js
+++ js/libs.js
@@ -1,7 +1,7 @@
 //by @demiurgosoft and @softwarejimenez
 /*========================= GESTION RATON========================= */
-//definimos un objeto de modificacion del modelo que recoja drag,dX,dY,AMORTIZATION,THETA,PHI.
-//con una funcion para gestionar
+//definimos un objeto de modificacion del modelo de evnetos de raton que recoja drag,dX,dY,AMORTIZATION,THETA,PHI.
+//con una funcion para gestionarlos
 var raton = {
     drag: false,
     dX: 0,
@@ -42,32 +42,33 @@ var raton = {
     }
 };
 
-
+//recoge un conjunto de funciones basicas.
 var LIBS = {
 
+    //crea el contexto de openGL
     getContext: function(CANVAS) {
         try {
-            var GL;
-            GL = CANVAS.getContext("experimental-webgl", {
+            return GL = CANVAS.getContext("experimental-webgl", {
                 antialias: true
             });
-            return GL;
         } catch (e) {
             alert("You are not webgl compatible :(");
             return false;
         }
     },
-
+    //situa el canvas en la ventana
     editCanvas: function(CANVAS, id) {
         CANVAS = document.getElementById(id);
         CANVAS.width = window.innerWidth;
         CANVAS.height = window.innerHeight;
         return CANVAS;
     },
+    //convierte de grado deg a grados Rad.
     degToRad: function(angle) {
         return (angle * Math.PI / 180);
     },
 
+    //devuevle matriz de proyeccion con un angulo con una distancia un left y un right
     get_projection: function(angle, a, zMin, zMax) {
         var tan = Math.tan(LIBS.degToRad(0.5 * angle)),
             A = -(zMax + zMin) / (zMax - zMin),
@@ -81,6 +82,7 @@ var LIBS = {
         ];
     },
 
+    //devuvelve matriz identidad
     get_I4: function() {
         return [1, 0, 0, 0,
             0, 1, 0, 0,
@@ -89,6 +91,7 @@ var LIBS = {
         ];
     },
 
+    //modifica a la matriz identidad
     set_I4: function(m) {
         m[0] = 1, m[1] = 0, m[2] = 0, m[3] = 0,
             m[4] = 0, m[5] = 1, m[6] = 0, m[7] = 0,
@@ -96,6 +99,7 @@ var LIBS = {
             m[12] = 0, m[13] = 0, m[14] = 0, m[15] = 1;
     },
 
+    //a partir de una matriz calcula la matriz de rotacion con un angulo en x
     rotateX: function(m, angle) {
         var c = Math.cos(angle);
         var s = Math.sin(angle);
@@ -110,7 +114,7 @@ var LIBS = {
         m[6] = m[6] * c + mv5 * s;
         m[10] = m[10] * c + mv9 * s;
     },
-
+    //a partir de una matriz calcula la matriz de rotacion con un angulo en y
     rotateY: function(m, angle) {
         var c = Math.cos(angle);
         var s = Math.sin(angle);
@@ -125,7 +129,7 @@ var LIBS = {
         m[6] = c * m[6] - s * mv4;
         m[10] = c * m[10] - s * mv8;
     },
-
+    //a partir de una matriz calcula la matriz de rotacion con un angulo en z
     rotateZ: function(m, angle) {
         var c = Math.cos(angle);
         var s = Math.sin(angle);
@@ -141,6 +145,7 @@ var LIBS = {
         m[9] = c * m[9] + s * mv8;
     },
 
+    //a partir de una matriz calcula la matriz de traslacion en el eje z.
     translateZ: function(m, t) {
         m[14] += t;
     }
@@ -148,6 +153,10 @@ var LIBS = {
 
 
 /*========================= SHADERS ========================= */
+//clase para gestion de los shader.
+//declaramos la matriz de proyeccion,visualizacion y de modelado
+//declaramos coordenadas de textrua y de posicion
+//creamos, linkamos y compilamos los shader.
 var shaders = {
     _Pmatrix: null,
     _Vmatrix: null,
@@ -210,110 +219,144 @@ var shaders = {
 
         GL.useProgram(SHADER_PROGRAM);
         GL.uniform1i(_sampler, 0);
-
     }
 };
 
 
 
 
-    /*========================= THE CUBE ========================= */
-    var modelo={
-        CUBE_VERTEX:null,
-        CUBE_FACES:null,
-        defModelo:function (GL) {
-            //POINTS :
-            var cube_vertex = [-1, -1, -1, 0, 0,
-                1, -1, -1, 1, 0,
-                1, 1, -1, 1, 1, -1, 1, -1, 0, 1,
+/*========================= THE CUBE ========================= */
+//definimos el modelo:
+//con las variables de identificadores de buffer de vertices y caras
+//definimos las coordenasda de los vertices y de textura
+//creamos los identificadores de los buffer
+var modelo={
+CUBE_VERTEX:null,
+CUBE_FACES:null,
+defModelo:function (GL) {
+    //POINTS :
+    var cube_vertex = [
+        -1, -1, -1,
+        0, 0,
+        1, -1, -1,
+        1, 0,
+        1, 1, -1,
+        1, 1,
+        -1, 1, -1,
+        0, 1,
 
-                -1, -1, 1, 0, 0,
-                1, -1, 1, 1, 0,
-                1, 1, 1, 1, 1, -1, 1, 1, 0, 1,
+        -1, -1, 1,
+        0, 0,
+        1, -1, 1,
+        1, 0,
+        1, 1, 1,
+        1, 1,
+        -1, 1, 1,
+        0, 1,
 
-                -1, -1, -1, 0, 0, -1, 1, -1, 1, 0, -1, 1, 1, 1, 1, -1, -1, 1, 0, 1,
+        -1, -1, -1,
+        0, 0,
+        -1, 1, -1,
+        1, 0,
+        -1, 1, 1,
+        1, 1,
+        -1, -1, 1,
+        0, 1,
 
-                1, -1, -1, 0, 0,
-                1, 1, -1, 1, 0,
-                1, 1, 1, 1, 1,
-                1, -1, 1, 0, 1,
+        1, -1, -1,
+        0, 0,
+        1, 1, -1,
+        1, 0,
+        1, 1, 1,
+        1, 1,
+        1, -1, 1,
+        0, 1,
 
-                -1, -1, -1, 0, 0, -1, -1, 1, 1, 0,
-                1, -1, 1, 1, 1,
-                1, -1, -1, 0, 1,
+        -1, -1, -1,
+        0, 0,
+        -1, -1, 1,
+        1, 0,
+        1, -1, 1,
+        1, 1,
+        1, -1,
+        -1, 0, 1,
+        -1, 1,
+        -1, 0, 0,
+        -1, 1, 1,
+        1, 0,
+        1, 1, 1,
+        1, 1,
+        1, 1, -1,
+        0, 1
+    ];
 
-                -1, 1, -1, 0, 0, -1, 1, 1, 1, 0,
-                1, 1, 1, 1, 1,
-                1, 1, -1, 0, 1
-            ];
+    modelo.CUBE_VERTEX = GL.createBuffer();
+    GL.bindBuffer(GL.ARRAY_BUFFER, modelo.CUBE_VERTEX);
+    GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(cube_vertex), GL.STATIC_DRAW);
 
-            modelo.CUBE_VERTEX = GL.createBuffer();
-            GL.bindBuffer(GL.ARRAY_BUFFER, modelo.CUBE_VERTEX);
-            GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(cube_vertex), GL.STATIC_DRAW);
+    //FACES :
+    var cube_faces = [
+        0, 1, 2,
+        0, 2, 3,
 
-            //FACES :
-            var cube_faces = [
-                0, 1, 2,
-                0, 2, 3,
+        4, 5, 6,
+        4, 6, 7,
 
-                4, 5, 6,
-                4, 6, 7,
+        8, 9, 10,
+        8, 10, 11,
 
-                8, 9, 10,
-                8, 10, 11,
+        12, 13, 14,
+        12, 14, 15,
 
-                12, 13, 14,
-                12, 14, 15,
+        16, 17, 18,
+        16, 18, 19,
 
-                16, 17, 18,
-                16, 18, 19,
+        20, 21, 22,
+        20, 22, 23
 
-                20, 21, 22,
-                20, 22, 23
+    ];
+    modelo.CUBE_FACES = GL.createBuffer();
+    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, modelo.CUBE_FACES);
+    GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(cube_faces), GL.STATIC_DRAW);
+}
 
-            ];
-            modelo.CUBE_FACES = GL.createBuffer();
-            GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, modelo.CUBE_FACES);
-            GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(cube_faces), GL.STATIC_DRAW);
-        }
-
-    };
-
-
-
-        /*========================= TEXTURES ========================= */
-        var texture={
-            get_texture:function (image_URL,GL) {
-
-
-                var image = new Image();
-
-                image.src = image_URL;
-                image.webglTexture = false;
-
-
-                image.onload = function(e) {
-
-                    var texture = GL.createTexture();
-
-                    GL.pixelStorei(GL.UNPACK_FLIP_Y_WEBGL, true);
+};
 
 
-                    GL.bindTexture(GL.TEXTURE_2D, texture);
 
-                    GL.texImage2D(GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA, GL.UNSIGNED_BYTE, image);
+/*========================= TEXTURES ========================= */
+//creamos la textura a paritr de una imagen.
+var texture={
+    get_texture:function (image_URL,GL) {
 
-                    GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
 
-                    GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST_MIPMAP_LINEAR);
+        var image = new Image();
 
-                    GL.generateMipmap(GL.TEXTURE_2D);
+        image.src = image_URL;
+        image.webglTexture = false;
 
-                    GL.bindTexture(GL.TEXTURE_2D, null);
 
-                    image.webglTexture = texture;
-                };
+        image.onload = function(e) {
 
-                return image;
-            }
+            var texture = GL.createTexture();
+
+            GL.pixelStorei(GL.UNPACK_FLIP_Y_WEBGL, true);
+
+
+            GL.bindTexture(GL.TEXTURE_2D, texture);
+
+            GL.texImage2D(GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA, GL.UNSIGNED_BYTE, image);
+
+            GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
+
+            GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST_MIPMAP_LINEAR);
+
+            GL.generateMipmap(GL.TEXTURE_2D);
+
+            GL.bindTexture(GL.TEXTURE_2D, null);
+
+            image.webglTexture = texture;
         };
+        return image;
+    }
+};
